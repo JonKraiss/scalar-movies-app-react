51ad0e3dd8dc7e8fa1949f132ff2b0774164da89
jkraiss <jonathan.kraiss@gmail.com>
2020-05-30 09:41:07 -0600

    Added serializer for the Movie model.

---
+++ moviesapp/movies/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'year', 'rated', 'released_on', 'genre', 'director', 'plot']
