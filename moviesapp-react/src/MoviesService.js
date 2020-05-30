dcac2a712d1447681d2d5532b29fc51a5bcd48c2
jkraiss <jonathan.kraiss@gmail.com>
2020-05-30 10:45:56 -0600

    Updated url configuration for movie API endpoints and adjusted the react app to use them.

--- moviesapp-react/src/MoviesService.js
+++ moviesapp-react/src/MoviesService.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:8000/movies/api';
 
 export default class MoviesService{
 
     constructor(){}
 
     getMoviesList() {
-        const url = `${API_URL}/api/movies/`;
+        const url = `${API_URL}`;
         return axios.get(url).then(response => response.data);
     }
     getMovie(id) {
-        const url = `${API_URL}/api/movies/${id}`;
+        const url = `${API_URL}/${id}`;
         return axios.get(url).then(response => response.data);
     }
     deleteMovie(movie){
-        const url = `${API_URL}/api/movies/${movie.id}`;
+        const url = `${API_URL}/${movie.id}`;
         return axios.delete(url);
     }
     createMovie(movie){
-        const url = `${API_URL}/api/movies/`;
+        const url = `${API_URL}`;
         return axios.post(url,movie);
     }
     updateMovie(movie){
-        const url = `${API_URL}/api/movies/${movie.id}`;
+        const url = `${API_URL}/${movie.id}`;
         return axios.put(url,movie);
     }
 }
--- moviesapp/movies/urls.py
+++ moviesapp/movies/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^create/$', view=views.MovieCreateView.as_view(), name='create'),
     url(r'^update/(?P<id>[0-9]+)/$', view=views.MovieUpdateView.as_view(), name='update'),
     url(r'^delete/(?P<id>[0-9]+)/$', view=views.MovieDeleteView.as_view(), name='delete'),
-    url(r'^api/movies/$', views.api_movies_list),
-    url(r'^api/movies/(?P<id>[0-9]+)$', views.api_movie),
+    url(r'^api/$', views.api_movies_list),
+    url(r'^api/(?P<movie_id>[0-9]+)$', views.api_movie),
 ]
