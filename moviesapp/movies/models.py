4c16b3d9313b942401798efa19cdc445e680f76b
jkraiss <jonathan.kraiss@gmail.com>
2020-05-30 21:27:25 -0600

    Added ratings model to django project.

--- moviesapp/movies/models.py
+++ moviesapp/movies/models.py
@@ -9,7 +9,6 @@ from django.utils.translation import ugettext_lazy as _
 class Movie(models.Model):
     title = models.CharField(_('Movie\'s title'), max_length=255)
     year = models.PositiveIntegerField(default=2019)
-    # Example: PG-13
     rated = models.CharField(max_length=64)
     released_on = models.DateField(_('Release Date'))
     genre = models.CharField(max_length=255)
@@ -17,7 +16,6 @@ class Movie(models.Model):
     plot = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
-    # Todo: add Rating models
 
     def __str__(self):
         return self.title
@@ -29,3 +27,21 @@ class Movie(models.Model):
         if Movie.objects.filter(title=self.title).exists():
             raise ValidationError('Movie with this Title already exists.')
         return super(Movie, self).clean()
+
+    def get_average_rating(self):
+        total = 0.0
+        average = 0.0
+        for r in self.rating_set.all():
+            total += 1
+            average += r.score
+        if total > 0:
+            average = average / total
+            return round(average, 1)
+        else:
+            return None
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    review = models.CharField(max_length=200)
+    score = models.IntegerField(default=0)
